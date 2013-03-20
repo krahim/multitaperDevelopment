18c9ffc15b425c3fc6c1e0e400b47946df2a9685
krahim <karim.rahim@gmail.com>
2013-03-19 20:07:02 -0400

    update

---
+++ multitaperHelper2.R
@@ -0,0 +1,26 @@
+## the comment had been updated here.
+.mw2wta <- function(sa, nfreq, nord,
+                    var, dt_, ev, evp=(1-ev),
+                    tol=.03, maxadaptiveiteration=100) {
+
+    ## this is equation (5.3) and (5.4) form 
+    ##   Thomson, D.J. Spectrum Estimation and Harmonic Analysis,
+    ##   Proceedings of the IEEE, 1982.
+
+    ## note that the weights are squared, they are |d_k(f)^2 from equation
+    ## (5.4)
+
+    out <- .Fortran("mw2wta", as.double(sa),
+                    wt=matrix(as.double(0), nfreq, nord),
+                    as.integer(nfreq), as.integer(nord),
+                    s=double(nfreq), as.double(ev), as.double(evp),
+                    dofs=double(nfreq), dofav=double(1),
+                    as.double(var), as.double(dt_),
+                    as.double(tol),
+                    as.integer(maxadaptiveiteration),
+                    mxiter=integer(1), aviter=double(1),
+                    PACKAGE='multitaper')
+    
+    return(list(s=out$s, wt=out$wt, dofs=out$dofs, dofav=out$dofav,
+                mxiter=out$mxiter, aviter=out$aviter))
+}
---
+++ utils2.R
@@ -0,0 +1,42 @@
+##     The multitaper R package (development code)
+##     Multitaper and spectral analysis package for R
+##     Copyright (C) 2013 Karim Rahim 
+##
+##     Written by Karim Rahim.
+##
+##     This file is part of the multitaper package for R.
+##     http://cran.r-project.org/web/packages/multitaper/index.html
+## 
+##     The multitaper package is free software: you can redistribute it and 
+##     or modify it under the terms of the GNU General Public License as 
+##     published by the Free Software Foundation, either version 2 of the 
+##     License, or any later version.
+##
+##     The multitaper package is distributed in the hope that it will be 
+##     useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+##     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU General Public License for more details.
+##
+##     You should have received a copy of the GNU General Public License
+##     along with multitaper.  If not, see <http://www.gnu.org/licenses/>.
+##
+##     If you wish to report bugs please contact the author:
+## 
+##     Karim Rahim
+##     karim.rahim@gmail.com
+##     Jeffery Hall, Queen's University, Kingston Ontario
+##     Canada, K7L 3N6
+
+
+
+## Utilities for and qi code
+
+sizeplotForRightAxis <- function() {
+    par(mar=c(5.1,4.1,4.1,4.1))
+}
+
+scaleFnToFit <- function(fnToFit, fnPlotted) {
+    const1 <- min(fnPlotted) - min(fnToFit)
+    scale1 <- diff(range(fnPlotted)) / diff(range(fnToFit))
+    const1+scale1*fnToFit
+}
