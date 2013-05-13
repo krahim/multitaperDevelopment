fbc1349e7a4a48bc70c7ea4d71ee78f02c2ef4c9
krahim <karim.rahim@gmail.com>
2013-05-13 12:04:12 -0400

    comment hilbert inverse fft

--- R/hilbert.R
+++ R/hilbert.R
@@ -12,9 +12,15 @@ hilbert <- function(dat, n=length(dat)) {
     h[c(1, nDiv2+1)] <- 1
 
     res <- res*h
-    ## is this n or length(dat) ...
+    ## is this n or length(dat) ?
+    ## no it is not.
     fft(res, inverse=TRUE)*1/n
 }
+
+## why /n and not length(dat)
+## fft(fft(c(1:10), inverse=TRUE))
+## fft(fft(c(1:10,rep(0,6)), inverse=TRUE))/10
+## fft(fft(c(1:10,rep(0,6)), inverse=TRUE))/16
 
 ## plot(Im(hilbert(sin(seq1))))
 ## lines(-cos(seq1))
