52dbd78972cecf01c9d642029e95feb9c16fa77c
krahim <karim.rahim@gmail.com>
2013-05-29 15:33:22 -0400

    note hilbert is only good for even n

--- R/hilbert.R
+++ R/hilbert.R
@@ -1,7 +1,11 @@
 ## this gives us essentially the matlab hilbert transform
 ## and we need the Im to get what we want.
+## this only works for even values of n only
+
 
 hilbert <- function(dat, n=length(dat)) {
+    stopifnot(n %% 2 == 0)
+    ## even values of n only at this time.
     
     dat <- c(dat, rep(0, n-length(dat)))
     res <- fft(dat)
