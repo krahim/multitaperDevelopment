6f84397ec2d4643c5e63b87fb0513ec76a276ae5
krahim <karim.rahim@gmail.com>
2013-06-27 17:26:15 -0400

    doh rm file with arCoefsToSdf

--- R/specTools.R
+++ R/specTools.R
@@ -3,17 +3,19 @@
 ## based on pw code. 
 ## moved form specTools.R in MultitaperDevelopmentPrivate June 20, 2013
 
-arCoefsToSdf <- function(arCoeffs, innovationsVariance, deltaT, nNonZeroFreqs) {
-    ## from rpaper package
-    ##Function to generate theoretical spectrum included in paper plot
-    ## based on pw code. 
+## ## this is in parametric.R which should be in the PWLISPR package, moved
 
-    prewhitenFilter <- c(1, -arCoeffs)
-    scratch <- c(prewhitenFilter, rep(0.0, 2 *
-                                      (nNonZeroFreqs - 1) -
-                                      length(prewhitenFilter)))
-    scratch <- fft(scratch)
-    numerator <- innovationsVariance * deltaT
-    resultSdf <- numerator / abs(scratch[1:(1+nNonZeroFreqs)])**2
-    return(resultSdf)
-}
+## arCoefsToSdf <- function(arCoeffs, innovationsVariance, deltaT, nNonZeroFreqs) {
+##     ## from rpaper package
+##     ##Function to generate theoretical spectrum included in paper plot
+##     ## based on pw code. 
+
+##     prewhitenFilter <- c(1, -arCoeffs)
+##     scratch <- c(prewhitenFilter, rep(0.0, 2 *
+##                                       (nNonZeroFreqs - 1) -
+##                                       length(prewhitenFilter)))
+##     scratch <- fft(scratch)
+##     numerator <- innovationsVariance * deltaT
+##     resultSdf <- numerator / abs(scratch[1:(1+nNonZeroFreqs)])**2
+##     return(resultSdf)
+## }
